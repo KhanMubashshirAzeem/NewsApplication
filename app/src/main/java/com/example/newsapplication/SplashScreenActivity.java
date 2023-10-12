b8ec7998de09e0954742f3ccbc80efd33aa7d443
Khan Mubashshir Azeem <khanmubashshir08@gmail.com>
2023-10-12 09:04:40 +0530

    Application is completed

---
+++ app/src/main/java/com/example/newsapplication/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.example.newsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // on below line we are configuring our window to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // on below line we are
+                // creating a new intent
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                // on below line we are
+                // starting a new activity.
+                startActivity(i);
+                // on the below line we are finishing
+                // our current activity.
+                finish();
+            }
+        }, 2000);
+
+
+
+    }
+}
